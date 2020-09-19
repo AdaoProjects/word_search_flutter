c91281c0829ae8bc793b04d1b96becb47725bef3
AdaoProjects <62807880+AdaoProjects@users.noreply.github.com>
2020-09-18 18:16:58 -0600

    fisrt

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:findthewords/screens/levels.dart';
 import 'package:findthewords/screens/stats.dart';
-import 'package:findthewords/screens/settings.dart';
 import 'package:findthewords/utilites/colors.dart';
 import 'package:findthewords/app_localizations.dart';
 
@@ -28,155 +27,137 @@ class _HomeState extends State<Home> {
                     .of(context)
                     .size
                     .width),
+            SizedBox(width:MediaQuery. of(context). size. width/20,
+                height: MediaQuery. of(context). size. height/20),
 
-            SizedBox(width:MediaQuery. of(context). size. width/15,
-                height: MediaQuery. of(context). size. height/15),
-            Row(
-              children:[
-                SizedBox(width: MediaQuery.of(context).size.width/5),
-                Container(
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width / 5,
-                  height: MediaQuery
-                      .of(context)
-                      .size
-                      .height / 10,
-                  child:
-                  IconButton(
-                    onPressed: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Level()),
-                      );
-                    },
-                    icon: Icon(Icons.play_arrow,
-                        color: Colors.white,
-                        size: MediaQuery
-                            .of(context)
-                            .size
-                            .height / 15),
-                    highlightColor: Colors.white,
-                  ),
-                  decoration:  BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: <Color>[GameColors.button_Background, Colors.black],),
-                    borderRadius: BorderRadius.all(Radius.circular(MediaQuery. of(context). size. height/5)),
-                  ),
-                ),
-                SizedBox(width: MediaQuery.of(context).size.width/5),
-                Container(
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width / 5,
-                  height: MediaQuery
-                      .of(context)
-                      .size
-                      .height / 10,
-                  child:
-                  IconButton(
-                    onPressed: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Stats()),
-                      );
-                    },
-                    icon: Icon(Icons.trending_up,
-                        color: Colors.white,
-                        size: MediaQuery
-                            .of(context)
-                            .size
-                            .height / 15),
-                    highlightColor: Colors.white,
-                  ),
-                  decoration:  BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: <Color>[GameColors.button_Background_Light, Colors.black],),
-                    borderRadius: BorderRadius.all(Radius.circular(MediaQuery. of(context). size. height/15)),
-                  ),
-                ),
-              ]
-            ),
-
-
-            SizedBox(width:MediaQuery. of(context). size. width/15,
-                height: MediaQuery. of(context). size. height/15),
-
-            Row(children:[
-              SizedBox(width: MediaQuery.of(context).size.width/5),
-              Container(
-                width: MediaQuery
-                    .of(context)
-                    .size
-                    .width / 5,
-                height: MediaQuery
-                    .of(context)
-                    .size
-                    .height / 10,
-                child:
-                IconButton(
-                  onPressed: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Settings()),
-                    );
-                  },
-                  icon: Icon(Icons.thumb_up,
-                      color: Colors.white,
-                      size: MediaQuery
-                          .of(context)
-                          .size
-                          .height / 15),
-                  highlightColor: Colors.white,
-                ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Level()),
+                );
+              } ,
+              color:Colors.black,
+              highlightColor: GameColors.button_Background_Light,
+              child:Ink(
                 decoration:  BoxDecoration(
                   gradient: LinearGradient(
-                    colors: <Color>[GameColors.button_Background_Light, Colors.black],),
+                    colors: <Color>[GameColors.button_Background, Colors.black],),
                   borderRadius: BorderRadius.all(Radius.circular(MediaQuery. of(context). size. height/15)),
+                ),
+                child: Container(
+                    constraints:  BoxConstraints(minWidth: MediaQuery. of(context). size. width*4/5, minHeight: MediaQuery. of(context). size. height/15), // min sizes for Material buttons
+                    alignment: Alignment.center,
+                    child: Text(AppLocalizations.of(context).translate("home_play"),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          fontSize: MediaQuery. of(context). size. height/15),
+                      textAlign: TextAlign.center,
+                    )
                 ),
               ),
-              SizedBox(width: MediaQuery.of(context).size.width/5),
-              Container(
-                width: MediaQuery
-                    .of(context)
-                    .size
-                    .width / 5,
-                height: MediaQuery
-                    .of(context)
-                    .size
-                    .height / 10,
-                child:
-                IconButton(
-                  onPressed: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Settings()),
-                    );
-                  },
-                  icon: Icon(Icons.settings,
-                      color: Colors.white,
-                      size: MediaQuery
-                          .of(context)
-                          .size
-                          .height / 15),
-                  highlightColor: Colors.white,
-                ),
+            ),
+
+            SizedBox(width:MediaQuery. of(context). size. width/15,
+                height: MediaQuery. of(context). size. height/15),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Level()),
+                );
+              } ,
+              color:Colors.black,
+              highlightColor: GameColors.button_Background,
+              child:Ink(
                 decoration:  BoxDecoration(
                   gradient: LinearGradient(
                     colors: <Color>[GameColors.button_Background_Light, Colors.black],),
                   borderRadius: BorderRadius.all(Radius.circular(MediaQuery. of(context). size. height/15)),
                 ),
+                child: Container(
+                    constraints:  BoxConstraints(minWidth: MediaQuery. of(context). size. width*4/5, minHeight: MediaQuery. of(context). size. height/15), // min sizes for Material buttons
+                    alignment: Alignment.center,
+                    child: Text(AppLocalizations.of(context).translate("home_share"),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          fontSize: MediaQuery. of(context). size. height/15),
+                      textAlign: TextAlign.center,
+                    )
+                ),
+              ),
+            ),
 
-              )
-            ])
+            SizedBox(width:MediaQuery. of(context). size. width/15,
+                height: MediaQuery. of(context). size. height/15),
 
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Stats()),
+                );
+              } ,
+              color:Colors.black,
+              highlightColor: GameColors.button_Background,
+              child:Ink(
+                decoration:  BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: <Color>[GameColors.button_Background_Light, Colors.black],),
+                  borderRadius: BorderRadius.all(Radius.circular(MediaQuery. of(context). size. height/15)),
+                ),
+                child: Container(
+                    constraints:  BoxConstraints(minWidth: MediaQuery. of(context). size. width*4/5, minHeight: MediaQuery. of(context). size. height/15), // min sizes for Material buttons
+                    alignment: Alignment.center,
+                    child: Text(AppLocalizations.of(context).translate("home_stats"),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          fontSize: MediaQuery. of(context). size. height/15),
+                      textAlign: TextAlign.center,
+                    )
+                ),
+              ),
+            ),
+            SizedBox(width:MediaQuery. of(context). size. width/15,
+                height: MediaQuery. of(context). size. height/15),
 
-
-
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Level()),
+                );
+              } ,
+              color:Colors.black,
+              highlightColor: GameColors.button_Background,
+              child:Ink(
+                decoration:  BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: <Color>[GameColors.button_Background_Light, Colors.black],),
+                  borderRadius: BorderRadius.all(Radius.circular(MediaQuery. of(context). size. height/15),
+                  ),
+                ),
+                child: Container(
+                    constraints:  BoxConstraints(minWidth: MediaQuery. of(context). size. width*4/5, minHeight: MediaQuery. of(context). size. height/15), // min sizes for Material buttons
+                    alignment: Alignment.center,
+                    child: Text(AppLocalizations.of(context).translate("home_settings"),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          fontSize: MediaQuery. of(context). size. height/15),
+                      textAlign: TextAlign.center,
+                    )
+                ),
+              ),
+            ),
           ],
-
         ),
       ),
     );
