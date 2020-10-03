4e63a3582b8aa4f13918dae1ff213b35b5b0e381
AdaoProjects <62807880+AdaoProjects@users.noreply.github.com>
2020-10-03 08:32:01 -0600

    fisrt

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -33,72 +33,92 @@ class _SettingsState extends State<Settings> {
 
                   SizedBox(height:MediaQuery.of(context).size.height/20),
 
-                  Row(children:[
-                    SizedBox(width:MediaQuery.of(context).size. width/3),
-                    Text(AppLocalizations.of(context).translate("settings_sound"),
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontStyle: FontStyle.italic,
-                            fontSize: MediaQuery. of(context). size. height/20)
+                  Column(
+
+                      children:[
+                    Row(children:[
+                      SizedBox(width:MediaQuery.of(context).size. width/3),
+                      Text(AppLocalizations.of(context).translate("settings_sound"),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                              fontSize: MediaQuery. of(context). size. height/20)
+                      ),
+                      Checkbox(
+                        hoverColor: Colors.white,
+                        activeColor: Colors.white,
+                        checkColor: GameColors.primary,
+                        value:checkValue,
+                        onChanged: (bool newValue)async{
+                          setState(() {
+                            checkValue=newValue;
+                          });
+                          if(checkValue==false){
+                            SharedPreferences prefs = await SharedPreferences.getInstance();
+                            prefs.setBool('has_Sounds', false);
+                            Navigator.of(context).pushNamed("/game_easy");
+                            Navigator.of(context).pushNamed("/game_medium");
+                            Navigator.of(context).pushNamed("/game_hard");
+                          }else{
+                            SharedPreferences prefs = await SharedPreferences.getInstance();
+                            prefs.setBool('has_Sounds', true);
+                            Navigator.of(context).pushNamed("/game_easy");
+                            Navigator.of(context).pushNamed("/game_medium");
+                            Navigator.of(context).pushNamed("/game_hard");
+                          }
+                        },
+                      )
+                    ]
                     ),
-                    Checkbox(
-                      hoverColor: Colors.white,
-                      activeColor: Colors.white,
-                      checkColor: GameColors.primary,
-                      value:checkValue,
-                      onChanged: (bool newValue)async{
-                        setState(() {
-                          checkValue=newValue;
-                        });
-                        if(checkValue==false){
-                          SharedPreferences prefs = await SharedPreferences.getInstance();
-                          prefs.setBool('has_Sounds', false);
-                          Navigator.of(context).pushNamed("/game_easy");
-                          Navigator.of(context).pushNamed("/game_medium");
-                          Navigator.of(context).pushNamed("/game_hard");
-                        }else{
-                          SharedPreferences prefs = await SharedPreferences.getInstance();
-                          prefs.setBool('has_Sounds', true);
-                          Navigator.of(context).pushNamed("/game_easy");
-                          Navigator.of(context).pushNamed("/game_medium");
-                          Navigator.of(context).pushNamed("/game_hard");
-                        }
+
+                    SizedBox(height:MediaQuery.of(context).size.height/25),
+
+                    DropdownButton(
+                      iconSize:MediaQuery. of(context). size. height/10 ,
+                      dropdownColor: Colors.white,
+                      underline: Text(AppLocalizations.of(context).translate("settings_language"),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                              fontSize: MediaQuery. of(context). size. height/20)
+                      ),
+                      items: Language.getLanguages().map((Language lang) {
+                        return DropdownMenuItem<Language>(
+                          value: lang,
+                          child: Text(lang.name,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontStyle: FontStyle.italic,
+                                  fontSize: MediaQuery. of(context). size. height/20)
+                          ),
+                        );
+                      }).toList(),
+
+                      onChanged: (Language val) {
+                        _selectedLanguage = val;
+                        MyApp.setLocale(context, Locale(val.languageCode,''));
                       },
-                    )
-                  ]
-                  ),
-                  SizedBox(height:MediaQuery.of(context).size.height/25),
-
-                  DropdownButton(
-                    iconSize:MediaQuery. of(context). size. height/10 ,
-                    dropdownColor: Colors.white,
-                    underline: Text(AppLocalizations.of(context).translate("settings_language"),
+                    ),
+                    SizedBox(height:MediaQuery.of(context).size.height/25),
+                    Text(AppLocalizations.of(context).translate("settings_theme"),
                         style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
                             fontStyle: FontStyle.italic,
                             fontSize: MediaQuery. of(context). size. height/20)
                     ),
-                    items: Language.getLanguages().map((Language lang) {
-                      return DropdownMenuItem<Language>(
-                        value: lang,
-                        child: Text(lang.name,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontStyle: FontStyle.italic,
-                                fontSize: MediaQuery. of(context). size. height/20)
-                        ),
-                      );
-                    }).toList(),
+                  ]
+                  )
 
-                    onChanged: (Language val) {
-                      _selectedLanguage = val;
-                      MyApp.setLocale(context, Locale(val.languageCode,''));
-                    },
-                  ),
-                ]
+          ]
+
+
+
+
+
             )
         )
     );
